fead716e6601ef5c14f9b7b700680f844e661a33
Wesley <wesleymarkpennock@gmail.com>
2011-07-04 16:52:22 -0500

    base

--- Python-Programming-101/Chapter 7.py
+++ Python-Programming-101/Chapter 7.py
@@ -10,4 +10,27 @@ while True:
 for line in f:
     print line.rstrip().upper()
 #7.2(Average Spam Confidence)
+while True:   
+    file = raw_input('Enter a filename: ')
+    try:
+      f = open(file, 'rU')
+      break
+    except:
+      print('Enter a valid filename: ')
+      continue
+spamlines=0
+spamtotal=0
+for line in f:
+    if not line.startswith('X-DSPAM-Confidence:'):
+        continue
+    colonposition = line.find(':') 
+    spamtotal = spamtotal + float(line[colonposition+1:]) 
+    spamlines = spamlines + 1 
+print 'Average spam confidence:', spamtotal/spamlines 
 #7.3(Easter Egg)
+file = raw_input('Enter a filename: ')
+while true:
+	if file== 'mbox-short.txt':
+		print('Was up, this is an egg fool!Haha jk!')  
+        
+    
